ee79bde516a87ffbc0b0b37f226925d0efabc683
Steven Watts <goldenjinjo@gmail.com>
2024-05-31 13:00:24 +1000

    add basic functionality for servos

--- Bellerophon-v3/lib/positionalServo/positionalServo.cpp
+++ Bellerophon-v3/lib/positionalServo/positionalServo.cpp
@@ -1,6 +1,60 @@
 #include "positionalServo.hpp"
 
-PositionalServo::PositionalServo() {}
+PositionalServo::PositionalServo() {
+    initialize();
+}
+
+void PositionalServo::initialize() {
+    // Assign pins to each servo
+    servos[0] = {Servo(), SERVO_PIN_A};
+    servos[1] = {Servo(), SERVO_PIN_B};
+    servos[2] = {Servo(), SERVO_PIN_C};
+    servos[3] = {Servo(), SERVO_PIN_D};
+
+    // Activate all servos
+    for (auto& servoObj : servos) {
+        activate(servoObj);
+    }
+}
+
+void PositionalServo::activate(ServoObject& servoObj) {
+    // Attach the servo to its pin
+    servoObj.servo.attach(servoObj.pin);
+}
+
+void PositionalServo::deactivate(ServoObject& servoObj) {
+    // Detach the servo from its pin
+    servoObj.servo.detach();
+}
+
+void PositionalServo::activateAll() {
+    // Activate all servos
+    for (auto& servoObj : servos) {
+        activate(servoObj);
+    }
+}
+
+void PositionalServo::deactivateAll() {
+    // Deactivate all servos
+    for (auto& servoObj : servos) {
+        deactivate(servoObj);
+    }
+}
+
+void PositionalServo::move(ServoObject& servoObj, uint8_t position) {
+    // Activate servo in case it was deactivated
+    activate(servoObj);
+
+    // Move the servo to the specified position
+    servoObj.servo.write(position);
+}
+
+void PositionalServo::stop(ServoObject& servoObj) {
+    // Move the servo to the zero position
+    move(servoObj, servoZeroValue);
+}
+
+
 
 
 
--- Bellerophon-v3/lib/positionalServo/positionalServo.hpp
+++ Bellerophon-v3/lib/positionalServo/positionalServo.hpp
@@ -1,14 +1,65 @@
-#include <servo.h>
+#include <Servo.h>
+#include "pinAssn.hpp"
+#include <array>
 
 class PositionalServo {
-    private:
+private:
+    // Methods
+    void initialize();
 
-    Servo servoA;
-    Servo servoB;
-    S
+    struct ServoObject {
+        Servo servo;
+        int pin;
+    };
+
+    // Members
+    // Array to hold Servo Objects
+    std::array<ServoObject, 4> servos;
+    // Default position for servo
+    uint8_t servoZeroValue = 0;
+
+public:
+    /**
+     * @brief Constructor for the PositionalServo class.
+     * Initializes the servos.
+     */
+    PositionalServo();
+
+    /**
+     * @brief Activates the specified servo by attaching it to its pin.
+     * @param servoObj The servo object to be activated.
+     */
+    void activate(ServoObject& servoObj);
+
+    /**
+     * @brief Deactivates the specified servo by detaching it from its pin.
+     * @param servoObj The servo object to be deactivated.
+     */
+    void deactivate(ServoObject& servoObj);
+
+    /**
+     * @brief Activates all servos by attaching them to their respective pins.
+     */
+    void activateAll();
+
+    /**
+     * @brief Deactivates all servos by detaching them from their respective pins.
+     */
+    void deactivateAll();
+
+    /**
+     * @brief Moves the specified servo to a given position.
+     * @param servoObj The servo object to be moved.
+     * @param position The position to move the servo to (0-180 degrees).
+     */
+    void move(ServoObject& servoObj, uint8_t position);
+
+    /**
+     * @brief Stops the specified servo by moving it to the zero position.
+     * @param servoObj The servo object to be stopped.
+     */
+    void stop(ServoObject& servoObj);
+};
 
 
 
-    public:
-    
-}
