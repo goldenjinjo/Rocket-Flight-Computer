a09015c85961951c3014ae42a5b5e5c852b61261
Steven Watts <goldenjinjo@gmail.com>
2024-07-05 11:25:28 +1000

    remove go to standby command and reduce sleep time in read mode

--- Python-Serial-Comm/utils/serialComm.py
+++ Python-Serial-Comm/utils/serialComm.py
@@ -157,10 +157,9 @@ def select_serial_action(string, ser):
                 print_debug(f"Unexpected error: {e}")
         else:
             print("Files will not be downloaded. Returning to standby.")
-            time.sleep(3)
+            time.sleep(1)
             try:
                 write_to_serial(ser, CANCEL_MSG_REQUEST)
-                write_to_serial(ser, GO_TO_STANDBY)
             except Exception as e:
                 print_debug(f"Error writing to serial port: {e}")
     
