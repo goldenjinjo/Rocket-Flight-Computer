e441b2807778a9e82f53c6856118ed6d37aad9a3
Steven Watts <goldenjinjo@gmail.com>
2024-08-08 11:33:26 +1000

    delete unnused outdated test files

--- tests/icm20948_test/src/simpsons4.cpp
+++
@@ -1,4 +0,0 @@
-float simpsons4(float timeStep, float der0, float der1, float der2, float derCurr, float valCurr){
-  float integratedVal = valCurr + timeStep/24*(55*derCurr-59*der2+37*der1-9*der0);
-  return integratedVal;
-}
